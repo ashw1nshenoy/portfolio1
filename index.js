d1f7cba644f31539fcecf8e41573383b4453b820
Ashwin <Ashwin>
2023-08-23 19:29:27 +0530

    js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function qr(){
     window.open("https://github.com/ashw1nshenoy/Qrcode",'_blank')
 }
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
