c8d77ff8b76b67d84f29ba3a7f0216ac1468c177
Ashwin <Ashwin>
2023-08-29 20:22:15 +0530

    social links

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+/*Social Links*/
+function instagram(){
+    window.open("https://www.instagram.com/___ashwin007_/")
+}
+function linkedin(){
+    window.open("https://www.linkedin.com/in/ashwin-shenoy-3937b1228")
+}
+function github(){
+    window.open("https://github.com/ashw1nshenoy")
+}
+/*Projects*/
 function qr(){
     window.open("https://github.com/ashw1nshenoy/Qrcode",'_blank')
 }
+/*Dark Mode*/
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-  }     
+}     
